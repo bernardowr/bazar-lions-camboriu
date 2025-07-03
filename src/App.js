9e6bf70779e53a3090ccbe58f1f57fe20428b157
Bernardo Werner <bernardo.wr@gmail.com>
2025-07-03 13:33:59 -0300

    docs: :memo: Textos atualizados.

--- src/App.js
+++ src/App.js
@@ -12,19 +12,22 @@ function App() {
       </header>
       <section className="banner">
         <h3>Produtos doados pela Receita Federal</h3>
-        <p>Participe do nosso bazar e contribua para ações solidárias!</p>
+        <p>Participe do nosso bazar e contribua com nossas ações solidárias!</p>
       </section>
       <section className="produtos">
         <ListaProdutos />
       </section>
       <section className="sobre">
         <h3>Sobre o Lions Clube Camboriú</h3>
-        <p>O Lions Clube Camboriú realiza ações solidárias em prol da comunidade. O bazar solidário conta com produtos doados pela Receita Federal, e toda a renda será revertida para projetos sociais.</p>
+        <p> O Lions Clube Camboriú é um grupo de voluntários dedicados que faz parte do Lions Clube International,
+           a maior organização de clubes de serviço do mundo. Nossos membros estão comprometidos em servir a nossa comunidade, 
+           focando em áreas como saúde ocular, meio ambiente e alívio da fome. Através de diversas ações e projetos, buscamos
+            fazer a diferença na vida das pessoas, guiados pelo lema "Nós Servimos".</p>
       </section>
       <footer className="rodape">
         <p style={{ fontWeight: 'bold', fontSize: 18 }}>Contato</p>
-        <p>Telefone: (47) 99999-9999</p>
-        <p>Email: lionscamboriu@email.com</p>
+        <p>Telefone: (47) 98834-8979</p>
+        <p>Email: lionsclubecamboriu@gmail.com</p>
         <p>Instagram: <a href="https://instagram.com/lionscamboriu" target="_blank" rel="noopener noreferrer" style={{ color: '#ffb81c' }}>@lionscamboriu</a></p>
         <p>Facebook: <a href="https://facebook.com/lionscamboriu" target="_blank" rel="noopener noreferrer" style={{ color: '#ffb81c' }}>facebook.com/lionscamboriu</a></p>
       </footer>
