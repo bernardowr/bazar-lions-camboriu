ac240d9a0a23396c68a692ca4877d84d919ec3df
Bernardo Werner <bernardo.wr@gmail.com>
2025-08-22 11:56:49 -0300

    feat: :sparkles: Inclusão de itens faltantes e edição de produtos.

--- src/App.js
+++ src/App.js
@@ -12,17 +12,30 @@ function App() {
       </header>
       <section className="banner">
         <h3>Produtos doados pela Receita Federal</h3>
-        <p>Participe do nosso bazar e contribua com nossas ações solidárias!</p>
+        <h4>
+          🗓 Data: 13/09/2025 |
+          📍 Local: OAB Camboriú - Rua Dr. Acácio Bernardes, 7 |
+          ⏰ Horário: Das 9h às 13h
+        </h4>
       </section>
       <section className="produtos">
         <ListaProdutos />
       </section>
       <section className="sobre">
+        <h3>Regras para Participação</h3>
+        <p>Compras apenas presenciais – não será possível reservar ou comprar online. As mercadoria serão vendidas somente
+          para pessoas físicas (CPF). Proibida revenda dos produtos adquiridos. Produtos sem garantia ou troca. Limite de
+          compra por CPF: R$ 2.500,00 por CPF (ou limite de quantidade por categoria). É obrigatório apresentar CPF e RG no
+          momento da compra. Entrada gratuita (haverá controle de acesso por ordem de chegada). Pagamento somente em dinheiro
+          e PIX.</p>
+        <p> Quantidade máxima para cada item: 👕 Vestuário: 12 unidades (3 unidades por tipo);  🧺 Artigos de bazar:
+          15 unidades (3 unidades por tipo); 📱 Equipamentos eletrônicos: 2 unidades por tipo; ⌚ Relógios de pulso: 5
+          unidades;💄 Perfumes e cosméticos: 5 unidades (3 unidades por tipo).</p>
         <h3>Sobre o Lions Clube Camboriú</h3>
         <p> O Lions Clube Camboriú é um grupo de voluntários dedicados que faz parte do Lions Clube International,
-           a maior organização de clubes de serviço do mundo. Nossos membros estão comprometidos em servir a nossa comunidade, 
-           focando em áreas como saúde ocular, meio ambiente e alívio da fome. Através de diversas ações e projetos, buscamos
-            fazer a diferença na vida das pessoas, guiados pelo lema "Nós Servimos".</p>
+          a maior organização de clubes de serviço do mundo. Nossos membros estão comprometidos em servir a nossa comunidade,
+          focando em áreas como saúde ocular, meio ambiente e alívio da fome. Através de diversas ações e projetos, buscamos
+          fazer a diferença na vida das pessoas, guiados pelo lema "Nós Servimos".</p>
       </section>
       <footer className="rodape">
         <p style={{ fontWeight: 'bold', fontSize: 18 }}>Contato</p>
